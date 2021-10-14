ef53b17f6a52f8f9a64c7c0128a7384bfc411aba
Shrishailyam Patil <47721022+ShrishailyamPatil@users.noreply.github.com>
2021-10-14 16:43:17 +0530

    Update RegexEx.java

--- CSS-Corp/Unit-1/Date,Regex/RegexEx.java
+++ CSS-Corp/Unit-1/Date,Regex/RegexEx.java
@@ -7,20 +7,25 @@ import java.util.regex.Matcher;
 public class RegexEx {
 
 	public static void main(String[] args){  
-	    Scanner sc=new Scanner(System.in);   
+	      Scanner sc=new Scanner(System.in);   
 	            
 	      System.out.println("Enter regex pattern:");   
 	      Pattern pattern = Pattern.compile(sc.nextLine());   //Pattern taken in
 	      System.out.println("Enter text:");     
 	      Matcher matcher = pattern.matcher(sc.nextLine());  // the word to match 
-	      boolean found = false;             
+	      boolean found = false;
+	      int b= 0;
 	      while (matcher.find()) {             
-	        System.out.println("I found the text "+matcher.group()+" starting at index "+ matcher.start()+" and ending at index "+matcher.end());         
-	        found = true;          
-	      }              
-	    if(!found){     
-	     System.out.println("No match found.");   
-	    }          
+	        System.out.println("The text "+matcher.group()+" starts at index "+ matcher.start()+" and endis at index "+matcher.end());         
+	        found = true; 
+		b++;
+	      } 
+		
+	      if(!found){     
+	       System.out.println("No match found.");   
+	     } else{
+		System.out.println("found."+ b + " times");
+	      }
 	   sc.close();   
 	}
 
